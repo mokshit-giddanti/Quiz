0ba3a293936c69b42d39df401cc61929d9dd371b
mokshit giddanti <mokshitlrao2016@gmail.com>
2024-10-13 22:16:03 +0530

    added result schema

--- backend/index.js
+++ backend/index.js
@@ -46,6 +46,13 @@ const QuizSchema = new mongoose.Schema({
 });
 const Quiz = mongoose.model('Quiz', QuizSchema);
 
+// Result Model
+const ResultSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    quiz: { type: mongoose.Schema.Types.ObjectId, ref: 'Quiz', required: true },
+    score: { type: Number, required: true },
+});
+const Result = mongoose.model('Result', ResultSchema);
 
 
 
